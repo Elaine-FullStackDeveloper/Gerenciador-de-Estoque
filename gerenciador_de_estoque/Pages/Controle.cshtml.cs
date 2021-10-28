d6178d2b880dea57a951542c256c61b101e79b79
Elaine <elaine.fullstackdeveloper@gmail.com>
2021-10-28 17:52:03 -0300

    Integrando a Tela de Controle

---
+++ gerenciador_de_estoque/Pages/Controle.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace gerenciador_de_estoque.Pages
+{
+    public class Controle : PageModel
+    {
+        private readonly ILogger<Controle> _logger;
+
+        public Controle(ILogger<Controle> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
